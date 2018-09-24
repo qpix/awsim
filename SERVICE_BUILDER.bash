dfbeba076f717e9e49508b50dec9f1afb73d56b6
Christian Åberg <christian.aberg@live.com>
2018-09-24 15:36:34 +0200

    added all aws services

--- SERVICE_BUILDER.bash
+++ SERVICE_BUILDER.bash
@@ -6,7 +6,7 @@ mkdir services
 for SERVICE_FILE in data/*/*/service-2.json; do
 	SERVICE_NAME=$(echo $SERVICE_FILE | cut -f2 -d/)
 	echo -n "awsim['$SERVICE_NAME'] = " | cat - $SERVICE_FILE > services/$SERVICE_NAME.js
-	echo "	<script src=\"services/$SERVICE_NAME.js\"></script>" >> tmp
+	echo "<script src=\"services/$SERVICE_NAME.js\"></script>" >> tmp
 done
 
 cat tmp | uniq > HTML_INCLUDES.html
