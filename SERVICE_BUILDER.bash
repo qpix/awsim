15bc0729f9973f620e09553091ed71c7893ccfbb
Christian Åberg <christian.aberg@live.com>
2018-09-24 15:27:10 +0200

    fixed bug

--- SERVICE_BUILDER.bash
+++ SERVICE_BUILDER.bash
@@ -6,5 +6,8 @@ mkdir services
 for SERVICE_FILE in data/*/*/service-2.json; do
 	SERVICE_NAME=$(echo $SERVICE_FILE | cut -f2 -d/)
 	echo -n "awsim['$SERVICE_NAME'] = " | cat - $SERVICE_FILE > services/$SERVICE_NAME.js
-	echo "	<script src=\"services/$SERVICE_NAME.js\"></script>" >> HTML_INCLUDES.html
+	echo "	<script src=\"services/$SERVICE_NAME.js\"></script>" >> tmp
 done
+
+cat tmp | uniq > HTML_INCLUDES.html
+rm tmp
